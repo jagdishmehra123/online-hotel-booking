6a4c2e784f152e5afa24d4c6de666f49fa1ab189
Harshada0611 <115868922+Harshada0611@users.noreply.github.com>
2023-05-12 11:35:18 +0530

    Update Video.jsx

--- user/src/components/video/Video.jsx
+++ user/src/components/video/Video.jsx
@@ -5,7 +5,7 @@ const Video = () => {
   return (
     <div className='main' >
         < video src={beachResort} autoPlay loop muted />
-        <div className='home-text' data-aos="flip-left" data-aos-delay="400" data-aos-easing="ease-in-out" 
+        <div className='home-text' data-aos="flip-left" data-aos-delay="700" data-aos-easing="ease-in-out" 
         >
              <h1 >Captivating Paradise</h1>
              <h4>for unwinding and reveling</h4>
