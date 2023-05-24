f35de9e383b01e3b85bebffa1d2c42bcecf58965
Harshada0611 <harshadaa1997@gmail>
2023-05-24 16:51:54 +0530

    resolved issues1

--- user/src/components/Root-Component/OurProperties/OurProperties.js
+++ user/src/components/Root-Component/OurProperties/OurProperties.js
@@ -14,7 +14,6 @@ const OurProperties = () => {
 
   const getPropertiesData = async () => {
     await axios.get(`/hotelbook`)
-    // await axios.get(`http://localhost:4001/hotelbook`)
 
       .then((res) => {
         console.log('property list', res.data)
